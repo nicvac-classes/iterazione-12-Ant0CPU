e6937ea611f9418e86362285ac97fdca3b4a8547
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-10-09 15:03:28 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,42 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int N;
+    float costo, risparmiI, risparmitot, risparmi, mancante, risparmiR;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    //inizializzazione variabili
+    N=0;
+
+    cout<<"Inseirire il costo della moto"<<endl;
+    cin>>costo;
+
+    cout<<"Inserire il numero di risparmi iniziali"<<endl;
+    cin>>risparmiI;
+
+    risparmitot=risparmiI;
+
+    while (risparmitot<costo)
+    {
+        cout<<"Inserire importo"<<endl;
+        cin>>risparmi;
+
+        risparmitot=risparmitot+risparmi;
+
+        if(risparmitot>costo)
+        {
+            mancante=0;
+        }
+        else
+        {
+            mancante=abs(costo-risparmitot);
+        }
+
+        cout<<"Mancano " <<mancante<< "€"<<endl;
+        N=N+1;
+    }
+
+    cout<<"Sono stati risparimiati "<<risparmitot<< "€" << " per "<<N<<" volte"<<endl;
+
+    risparmiR=abs(costo-risparmitot);
+    cout<<"Risparmi rimanenti " <<risparmiR<< "€"<<endl;
+}
